690cdd05d66c3968183ab00bf4f479d5aa13cb03
Andrew Feeney <>
2020-04-10 20:47:47 +0100

    initial commit

---
+++ GlasgowAstro.Obsy/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(GlasgowAstro.Obsy.DataGrabber.Startup))]
+
+namespace GlasgowAstro.Obsy.DataGrabber
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+        }
+    }
+}
