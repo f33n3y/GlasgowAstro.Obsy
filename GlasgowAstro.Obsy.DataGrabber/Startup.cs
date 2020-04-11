005463cccd0e05476b32c1d85c28474795e7a4b8
f33n3y <f33n3y_a@hotmail.co.uk>
2020-04-12 00:09:17 +0100

    Setup IConfiguration for DataGrabber project

--- GlasgowAstro.Obsy.DataGrabber/Startup.cs
+++ GlasgowAstro.Obsy.DataGrabber/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 [assembly: FunctionsStartup(typeof(GlasgowAstro.Obsy.DataGrabber.Startup))]
 
@@ -8,7 +10,18 @@ namespace GlasgowAstro.Obsy.DataGrabber
     public class Startup : FunctionsStartup
     {
         public override void Configure(IFunctionsHostBuilder builder)
-        {
+        {           
+            var config = new ConfigurationBuilder();
+            config.SetBasePath(Environment.CurrentDirectory);
+            config.AddJsonFile("appsettings.json", false);
+            if (Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") == "Development")
+            {
+                config.AddUserSecrets<Startup>(false);
+            }
+            config.AddEnvironmentVariables();            
+
+            builder.Services.AddSingleton<IConfiguration>(config.Build());
+
             builder.Services.AddHttpClient();
         }
     }
