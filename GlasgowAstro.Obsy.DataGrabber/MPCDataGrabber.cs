7f9d95d4839244a9d5be48b55459d9b4d1f74122
f33n3y <f33n3y_a@hotmail.co.uk>
2020-04-12 00:08:42 +0100

    Moved some stuff to app settings.

--- GlasgowAstro.Obsy.DataGrabber/MPCDataGrabber.cs
+++ GlasgowAstro.Obsy.DataGrabber/MPCDataGrabber.cs
@@ -1,5 +1,6 @@
 using GlasgowAstro.Obsy.DataGrabber.Models;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -8,19 +9,20 @@ using System.IO.Compression;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using GlasgowAstro.Obsy.Data;
+using Microsoft.AspNetCore.Mvc.Formatters.Internal;
 
 namespace GlasgowAstro.Obsy.DataGrabber
 {
     public class MPCDataGrabber
-    {   
-        private readonly string DataUrl = Environment.GetEnvironmentVariable("ObsDataUrl");
-        private readonly string FolderName = Environment.GetEnvironmentVariable("ObsDataFolderName");
-        private readonly string FileName = Environment.GetEnvironmentVariable("ObsDataFileName");
+    {
         private readonly HttpClient _client;
+        private readonly IConfiguration _config;        
 
-        public MPCDataGrabber(HttpClient client)
+        public MPCDataGrabber(HttpClient client, IConfiguration configuration)
         {
             _client = client;
+            _config = configuration;
         }
 
         [FunctionName("MPCDataGrabber")]
@@ -28,32 +30,40 @@ namespace GlasgowAstro.Obsy.DataGrabber
         {
             log.LogInformation($"MPCDataGrabber Timer trigger function executed at: {DateTime.Now}");
 
-            var tempDir = Path.Combine(Path.GetTempPath(), FolderName);
+            var dataUrl = _config["ObsDataUrl"];
+            var folderName = _config["ObsDataFolderName"];
+            var fileName = _config["ObsDataFileName"];
 
-            if (!Directory.Exists(tempDir))
+            if (_config["DownloadData"] == "true")
             {
-                Directory.CreateDirectory(tempDir);
-            }
-
-            var res = await _client.GetStreamAsync(DataUrl);
-            log.LogInformation("Got Stream");
-
-            using (FileStream decompressedFileStream = File.Create($"{tempDir}\\{FileName}"))
-            {
-                using (GZipStream decompressionStream = new GZipStream(res, CompressionMode.Decompress))
+                var tempDir = Path.Combine(Path.GetTempPath(), folderName);
+                if (!Directory.Exists(tempDir))
                 {
-                    await decompressionStream.CopyToAsync(decompressedFileStream);
-                    log.LogInformation("File decompressed");
+                    Directory.CreateDirectory(tempDir);
                 }
+
+                var res = await _client.GetStreamAsync(dataUrl);
+                log.LogInformation("Got Stream");
+
+                using (FileStream decompressedFileStream = File.Create($"{tempDir}\\{fileName}"))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(res, CompressionMode.Decompress))
+                    {
+                        await decompressionStream.CopyToAsync(decompressedFileStream);
+                        log.LogInformation("File decompressed");
+                    }
+                }
+
+                //using (FileStream fs = File.OpenRead($"{tempDir}\\{fileName}"))
+                //{
+                //    var asteroids = await JsonSerializer.DeserializeAsync<List<Asteroid>>(fs);
+                //}
             }
 
-            // Read file and deserialize 
-            //using (FileStream fs = File.OpenRead($"{tempDir}\\{FileName}"))
-            //{
-            //    var asteroids = await JsonSerializer.DeserializeAsync<List<Asteroid>>(fs);
-            //}
-
-            // Store...
         }
     }
 }
+
+
+
+
