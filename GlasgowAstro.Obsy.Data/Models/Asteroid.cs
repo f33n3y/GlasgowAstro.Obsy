35b807709469c829d511ffec5d710b58070bc649
f33n3y <f33n3y_a@hotmail.co.uk>
2020-04-12 23:31:26 +0100

    Create data model for storing to mongodb

--- GlasgowAstro.Obsy.Data/Models/Asteroid.cs
+++ GlasgowAstro.Obsy.Data/Models/Asteroid.cs
@@ -1,13 +1,28 @@
 ﻿using GlasgowAstro.Obsy.Data.Attributes;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace GlasgowAstro.Obsy.Data.Models
 {
     [BsonCollection("data")]
     public class Asteroid : Document
     {
+        /// <summary>
+        /// Name, if asteroid has received one
+        /// </summary>
+        [BsonElement("Name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Name of orbit computer (be it a person or machine)
+        /// </summary>
+        [BsonElement("Computer")]
         public string Computer { get; set; }
+
+        /// <summary>
+        /// Absolute magnitude
+        /// </summary>
+        [BsonElement("H")]
+        public float AbsoluteMagnitude { get; set; }
 
         // TODO ....
     }
