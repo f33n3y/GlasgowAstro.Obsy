e31ea34d776c13d39838c2563d5d9b44e43fc873
f33n3y <f33n3y_a@hotmail.co.uk>
2020-04-13 01:59:14 +0100

    Tidy ups and test code in datagrabber function

--- GlasgowAstro.Obsy.DataGrabber/MPCDataGrabber.cs
+++ GlasgowAstro.Obsy.DataGrabber/MPCDataGrabber.cs
@@ -1,10 +1,11 @@
 using AutoMapper;
-using GlasgowAstro.Obsy.Data;
+using GlasgowAstro.Obsy.Data.Abstractions;
 using GlasgowAstro.Obsy.Data.Models;
 using GlasgowAstro.Obsy.DataGrabber.Contracts;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,13 +17,13 @@ namespace GlasgowAstro.Obsy.DataGrabber
     public class MPCDataGrabber
     {
         private readonly IConfiguration _config;
-        private readonly IRepository<Data.Models.Asteroid> _asteroidRepository;
+        private readonly IMongoRepository<Data.Models.Asteroid> _asteroidRepository;
         private readonly IMapper _mapper;
         private readonly IFileDownloader _downloader;
 
-        public MPCDataGrabber(IConfiguration config, IRepository<Data.Models.Asteroid> asteroidRepository,
+        public MPCDataGrabber(IConfiguration config, IMongoRepository<Data.Models.Asteroid> asteroidRepository,
             IMapper mapper, IFileDownloader downloader)
-        {
+        {   
             _config = config;
             _asteroidRepository = asteroidRepository;
             _mapper = mapper;
@@ -33,18 +34,22 @@ namespace GlasgowAstro.Obsy.DataGrabber
         public async Task Run([TimerTrigger("0 0 0 * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
         {
             var filePath = await _downloader.DownloadAsync();
+            log.LogInformation("File downloaded");
 
             if (!string.IsNullOrWhiteSpace(filePath))
             {
                 // Read file, deserialize, map and store. TODO Move to class.
                 IEnumerable<Models.Asteroid> asteroids;
-                using (FileStream fs = File.OpenRead(filePath))
+                using (FileStream fileStream = File.OpenRead(filePath))
                 {
-                    asteroids = await JsonSerializer.DeserializeAsync<List<Models.Asteroid>>(fs); // Do this in batches?                
-                    log.LogInformation("File deserialized");
-                    ICollection<Asteroid> documents = _mapper.Map<IEnumerable<Models.Asteroid>, IEnumerable<Data.Models.Asteroid>>(asteroids).ToList();
-                    //await _asteroidRepository.InsertManyAsync(documents); // TODO Make this InsertOrUpdate
+                    asteroids = await JsonSerializer.DeserializeAsync<List<Models.Asteroid>>(fileStream); // Do this in batches?                
+                    log.LogInformation("File deserialized");                   
                 }
+
+                //await _asteroidRepository.DeleteManyAsync(Builders<Asteroid>.Filter.Empty); 
+                ICollection<Asteroid> documents = _mapper.Map<IEnumerable<Models.Asteroid>, IEnumerable<Asteroid>>(asteroids).ToList();
+                //await _asteroidRepository.InsertManyAsync(documents); // TODO Make this InsertOrUpdate "upsert"   
+                log.LogInformation("DB updated");
             }
         }
     }
