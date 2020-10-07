dd79e88a5d5520c87335b8c285f8aef61dc83484
f33n3y <f33n3y_a@hotmail.co.uk>
2020-10-08 00:15:15 +0100

    Added service project

---
+++ GlasgowAstro.Obsy.Services/Abstractions/IAsteroidService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlasgowAstro.Obsy.Services.Abstractions
+{
+    public interface IAsteroidService
+    {
+    }
+}
---
+++ GlasgowAstro.Obsy.Services/AsteroidService.cs
@@ -0,0 +1,11 @@
+﻿using GlasgowAstro.Obsy.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlasgowAstro.Obsy.Services
+{
+    public class AsteroidService : IAsteroidService
+    {
+    }
+}
