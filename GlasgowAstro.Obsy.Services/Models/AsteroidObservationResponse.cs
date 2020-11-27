37317dd65e76f21ab9f9f6f9fa6a979c388ed2a9
f33n3y <f33n3y_a@hotmail.co.uk>
2020-11-27 23:13:56 +0000

    Start of response models for asteroid observations

---
+++ GlasgowAstro.Obsy.Services/Models/AsteroidObservationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlasgowAstro.Obsy.Services.Models
+{
+    public class AsteroidObservationResponse
+    {
+        public List<AsteroidObservation> Observations { get; set; }
+    }
+}
