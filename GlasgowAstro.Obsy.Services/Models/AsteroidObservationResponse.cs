664cbfe5547df9f27a1f04b34952b0ca5010fba5
f33n3y <f33n3y_a@hotmail.co.uk>
2020-12-06 21:01:31 +0000

    Add rawtest property for testing raw response before completing parsing work

--- GlasgowAstro.Obsy.Services/Models/AsteroidObservationResponse.cs
+++ GlasgowAstro.Obsy.Services/Models/AsteroidObservationResponse.cs
@@ -6,6 +6,8 @@ namespace GlasgowAstro.Obsy.Services.Models
 {
     public class AsteroidObservationResponse
     {
+        public string RawTest { get; set; }
+
         public List<AsteroidObservation> Observations { get; set; }
     }
 }
