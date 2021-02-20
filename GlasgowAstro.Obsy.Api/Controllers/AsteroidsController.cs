ebbbeff77b62e686e59a9ac9274c324fbf33f439
f33n3y <f33n3y_a@hotmail.co.uk>
2021-02-20 22:14:30 +0000

    Removed unused method from AsteroidController. Updated FindObservationAsync method to use ObservationResponse model instead of string. Removed check for valid modelstate as bad requests handled automatically

--- GlasgowAstro.Obsy.Api/Controllers/AsteroidsController.cs
+++ GlasgowAstro.Obsy.Api/Controllers/AsteroidsController.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
 using GlasgowAstro.Obsy.Api.Models.Request;
+using GlasgowAstro.Obsy.Api.Models.Response;
 using GlasgowAstro.Obsy.Services.Abstractions;
 using GlasgowAstro.Obsy.Services.Models.Request;
-using GlasgowAstro.Obsy.Services.Models.Response;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,73 +18,35 @@ namespace GlasgowAstro.Obsy.Api.Controllers
     [Authorize]
     public class AsteroidsController : ControllerBase
     {
-        private readonly IAsteroidDataService _asteroidService;
         private readonly IAsteroidObservationService _observationService;
         private readonly IMapper _mapper;
 
         public AsteroidsController(IAsteroidDataService asteroidService, IAsteroidObservationService observationService, IMapper mapper)
         {
-            _asteroidService = asteroidService;
             _observationService = observationService;
             _mapper = mapper;
         }
-
-        // GET api/asteroids/hello
-        [HttpGet("hello")]
-        public string HelloFriend()
-        {
-            return "Hello friend";
-        }
-
-        // GET api/asteroids/{name}
-        //[HttpGet("{name}")]
-        //[ProducesResponseType(StatusCodes.Status200OK)]
-        //[ProducesResponseType(StatusCodes.Status404NotFound)]
-        //public async Task<ActionResult<AsteroidDataResponse>> FindAsteroidAsync(string name) // TODO validation + request model
-        //{
-        //    try
-        //    {
-        //        var asteroid = await _asteroidService.FindAsteroidByNameAsync(name);
-
-        //        if (asteroid == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        return Ok(asteroid);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        // TODO logging...
-        //        return NotFound();
-        //    }
-        //}
 
         // GET api/asteroids/observations
         [HttpGet("/observations")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<string>> FindObservationsAsync([FromQuery] ObservationRequest obsySearchRequest) 
+        public async Task<ActionResult<ObservationResponse>> FindObservationsAsync([FromQuery] ObservationRequest obsySearchRequest) 
         {
             try
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest();
-                }
+                var serviceRequestModel = _mapper.Map<AsteroidObservationServiceRequest>(obsySearchRequest);                
+                var serviceResponse = await _observationService.GetObservationsAsync(serviceRequestModel);
 
-                var serviceRequestModel = _mapper.Map<ObservationServiceRequest>(obsySearchRequest);
-
-                // TODO Return response model 
-                var observation = await _observationService.GetObservationsAsync(serviceRequestModel);
-
-                if (observation == null)
+                if (serviceResponse == null || serviceResponse?.Observations.Count < 1)
                 {
                     return NotFound();
                 }
 
-                return Ok(observation.RawTest);
+                var observationResponse = _mapper.Map<ObservationResponse>(serviceResponse);
+
+                return Ok(observationResponse);
 
             }
             catch (Exception e)
