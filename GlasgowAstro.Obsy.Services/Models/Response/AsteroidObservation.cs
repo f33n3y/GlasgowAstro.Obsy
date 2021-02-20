0ec446a0828bf79815c64c44675baadea1236376
f33n3y <f33n3y_a@hotmail.co.uk>
2021-02-20 22:15:19 +0000

    Added JSON property names to AsteroidObservation to help with deserialisation

--- GlasgowAstro.Obsy.Services/Models/Response/AsteroidObservation.cs
+++ GlasgowAstro.Obsy.Services/Models/Response/AsteroidObservation.cs
@@ -1,20 +1,27 @@
-﻿namespace GlasgowAstro.Obsy.Services.Models.Response
+﻿using System.Text.Json.Serialization;
+
+namespace GlasgowAstro.Obsy.Services.Models.Response
 {
     // TODO Fix this so it isn't all strings
     public class AsteroidObservation
     {
         public string Designation { get; set; }
 
+        [JsonPropertyName("discovery_flag")]
         public string DiscoveryFlag { get; set; }
 
         public string Number { get; set; }
 
+        [JsonPropertyName("object_type")]
         public string ObjectType { get; set; }
 
+        [JsonPropertyName("observation_date")]
         public string ObservationDate { get; set; }
 
+        [JsonPropertyName("observatory_code")]
         public string ObservatoryCode { get; set; }
 
+        [JsonPropertyName("original_record")]
         public string OriginalRecord { get; set; }
     }
 }
