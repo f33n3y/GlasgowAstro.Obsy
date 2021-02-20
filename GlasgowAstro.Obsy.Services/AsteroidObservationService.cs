d7c725b4f351217758572ab923538e14df69b1fc
f33n3y <f33n3y_a@hotmail.co.uk>
2021-02-20 22:15:57 +0000

    Changed from IHttpClientFactory to HttpClient. Tidied logic in service method.

--- GlasgowAstro.Obsy.Services/AsteroidObservationService.cs
+++ GlasgowAstro.Obsy.Services/AsteroidObservationService.cs
@@ -5,46 +5,48 @@ using Microsoft.AspNetCore.WebUtilities;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Linq;
 
 namespace GlasgowAstro.Obsy.Services
 {
     public class AsteroidObservationService : IAsteroidObservationService
     {
-        private readonly IHttpClientFactory _clientFactory;
+        private readonly HttpClient _httpClient;
 
-        public AsteroidObservationService(IHttpClientFactory clientFactory)
+        public AsteroidObservationService(HttpClient httpClient)
         {
-            _clientFactory = clientFactory;
+            _httpClient = httpClient;
         }
 
-        // TODO Request validation + automap. Return AsteroidObservationResponse.
-        public async Task<AsteroidObservationResponse> GetObservationsAsync(ObservationServiceRequest asteroidObservationRequest)
+        public async Task<AsteroidObservationServiceResponse> GetObservationsAsync(AsteroidObservationServiceRequest asteroidObservationRequest)
         {
+            if (asteroidObservationRequest == null || !string.IsNullOrWhiteSpace(asteroidObservationRequest.Number))
+            {
+                return new AsteroidObservationServiceResponse { Observations = new List<AsteroidObservation>() };
+            }
 
-            // TODO Map request object to MpcClient request...
-
+            // TODO Tidy this up
             var queryArguments = new Dictionary<string, string>()
             {
                 { "number", asteroidObservationRequest.Number },
                 { "object_type", "M" },
                 { "table", "observations" },
-                { "limit", "3" },
+                { "limit", "5" },
                 { "order_by_desc", "observation_date" },
             };
 
             var requestUri = QueryHelpers.AddQueryString("/search_db", queryArguments);
 
-            var client = _clientFactory.CreateClient("MpcClient");
-            var result = await client.GetAsync(requestUri);
-
-            var observationResponse = new AsteroidObservationResponse();
+            var result = await _httpClient.GetAsync(requestUri);
 
             if (result.IsSuccessStatusCode)
-            {
-                observationResponse.RawTest = await result.Content.ReadAsStringAsync();
+            {                
+                var observations = JsonSerializer.Deserialize<List<AsteroidObservation>>(await result.Content.ReadAsStringAsync());
+                return new AsteroidObservationServiceResponse { Observations = observations };
             }
 
-            return observationResponse;
+            return new AsteroidObservationServiceResponse { Observations = new List<AsteroidObservation>() };
         }
 
     }
