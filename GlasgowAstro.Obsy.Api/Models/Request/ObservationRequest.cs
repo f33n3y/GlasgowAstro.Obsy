d35a749c7bb41e139d1c4a00956e972e92cf5040
f33n3y <f33n3y_a@hotmail.co.uk>
2021-02-20 22:08:26 +0000

    Remove NumObservations property from ObservationRequest model

--- GlasgowAstro.Obsy.Api/Models/Request/ObservationRequest.cs
+++ GlasgowAstro.Obsy.Api/Models/Request/ObservationRequest.cs
@@ -7,9 +7,5 @@ namespace GlasgowAstro.Obsy.Api.Models.Request
         [Required]
         [RegularExpression("^[0-9]*$")]  // TODO Expand this regex to cater for other object types
         public string Number { get; set; }
-
-        [Required]
-        [Range(byte.MinValue, byte.MaxValue)]        
-        public byte NumObservations { get; set; }
     }
 }
