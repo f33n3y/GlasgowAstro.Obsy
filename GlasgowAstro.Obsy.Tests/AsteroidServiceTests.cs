20552059d5f604f883e6dde96f0f38dea45a5e03
f33n3y <f33n3y_a@hotmail.co.uk>
2021-04-01 23:18:53 +0100

    Install AutoFixture, FluentAssertions and NSubstitute into test project

--- GlasgowAstro.Obsy.Tests/AsteroidServiceTests.cs
+++ GlasgowAstro.Obsy.Tests/AsteroidServiceTests.cs
@@ -1,10 +1,20 @@
+using GlasgowAstro.Obsy.Services.Abstractions;
 using System;
 using Xunit;
+using FluentAssertions;
+using NSubstitute;
 
 namespace GlasgowAstro.Obsy.Tests
 {
     public class AsteroidServiceTests
     {
+        private readonly IAsteroidService _sut;
+
+        public AsteroidServiceTests()
+        {
+            _sut = Substitute.For<IAsteroidService>();
+        }
+
         [Fact]
         public void Test1()
         {
