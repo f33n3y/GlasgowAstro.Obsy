c47da191e8428f00aa811d145a4933a3577d96b0
f33n3y <f33n3y_a@hotmail.co.uk>
2021-04-01 23:05:45 +0100

    Changed test project from NUnit to XUnit

---
+++ GlasgowAstro.Obsy.Tests/AsteroidServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GlasgowAstro.Obsy.Tests
+{
+    public class AsteroidServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
