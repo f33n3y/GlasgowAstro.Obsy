9776214f6b1288df35fabdf066e70eaff2f9448e
f33n3y <f33n3y_a@hotmail.co.uk>
2021-04-01 23:53:16 +0100

    Create Fixture object in test class and Substitute for IHttpClientFactory

--- GlasgowAstro.Obsy.Tests/AsteroidServiceTests.cs
+++ GlasgowAstro.Obsy.Tests/AsteroidServiceTests.cs
@@ -3,22 +3,40 @@ using System;
 using Xunit;
 using FluentAssertions;
 using NSubstitute;
+using AutoFixture;
+using System.Threading.Tasks;
+using GlasgowAstro.Obsy.Services.Models.Request;
+using GlasgowAstro.Obsy.Services.Models.Response;
+using System.Net.Http;
 
 namespace GlasgowAstro.Obsy.Tests
 {
     public class AsteroidServiceTests
     {
         private readonly IAsteroidService _sut;
+        private readonly IHttpClientFactory _mockHttpClientFactory;
+        private readonly IFixture _fixture = new Fixture();
 
         public AsteroidServiceTests()
         {
             _sut = Substitute.For<IAsteroidService>();
+            _mockHttpClientFactory = Substitute.For<IHttpClientFactory>();
         }
 
         [Fact]
-        public void Test1()
+        public async Task GetOrbitDataAsync_ShouldReturnResponseWithMagnitude_WhenAsteroidNumberValid()
         {
+            // Arrange
+            //const string asteroidNum = "134340";
+            //var apiReponse = _fixture.Create<AsteroidOrbitDataResponse>();
+            //var request = new AsteroidOrbitDataRequest { Number = asteroidNum };
 
+            
+            // Act
+            //var result = await _sut.GetOrbitDataAsync()
+
+            // Assert
         }
+
     }
 }
