1571accd24c77521ef806d4c73bdc4f28c019e28
f33n3y <f33n3y_a@hotmail.co.uk>
2021-11-07 18:21:07 +0000

    Update OrbitRequest object

--- GlasgowAstro.Obsy.Api/Models/Request/OrbitRequest.cs
+++ GlasgowAstro.Obsy.Api/Models/Request/OrbitRequest.cs
@@ -7,5 +7,8 @@ namespace GlasgowAstro.Obsy.Api.Models.Request
 {
     public class OrbitRequest
     {
+        [Required]
+        [RegularExpression("^[0-9]*$")]  // TODO Expand this regex to cater for other object types
+        public string Number { get; set; }
     }
 }
