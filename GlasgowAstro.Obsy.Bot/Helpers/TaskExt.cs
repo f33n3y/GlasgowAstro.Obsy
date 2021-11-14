b13b260bac806f13654ce1a5bccbd1e41dd02a75
f33n3y <f33n3y_a@hotmail.co.uk>
2021-11-14 00:37:30 +0000

    Created Task helper to make it easier to get results of WhenAll

---
+++ GlasgowAstro.Obsy.Bot/Helpers/TaskExt.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace GlasgowAstro.Obsy.Bot.Helpers
+{
+    public static class TaskExt
+    {
+        public static async Task<(Task<T0>, Task<T1>)> WhenAll<T0, T1>(Task<T0> task0, Task<T1> task1)
+        {
+            await Task.WhenAll(task0, task1).ConfigureAwait(false);
+            return (task0, task1);
+        }
+    }
+}
