0f263952bb8e764d377fc5f2ad89b4880b941576
f33n3y <f33n3y_a@hotmail.co.uk>
2021-11-26 22:28:51 +0000

    Add MaxLength to request models

--- GlasgowAstro.Obsy.Api/Models/Request/OrbitRequest.cs
+++ GlasgowAstro.Obsy.Api/Models/Request/OrbitRequest.cs
@@ -6,6 +6,7 @@ namespace GlasgowAstro.Obsy.Api.Models.Request
     {
         [Required]
         [RegularExpression("^[0-9]*$")]  // TODO Expand this regex to cater for other object types
+        [MaxLength(10)]
         public string Number { get; set; }
     }
 }
